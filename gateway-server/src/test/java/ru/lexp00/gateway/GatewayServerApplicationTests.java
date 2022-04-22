f3272a18c648bbd630501aafe6be5460e70ea94a
alvlap <alvlap@mail.ru>
2022-04-22 11:02:28 +0300

    refactor microservice: add gateway-server

---
+++ gateway-server/src/test/java/ru/lexp00/gateway/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lexp00.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
